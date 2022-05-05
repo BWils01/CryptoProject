f514b0ca48455208c5c32c9b3a368ad51909c13f
Benjamin Wilson <Ben.Wilson@trojans.dsu.edu>
2022-05-04 22:36:19 -0500

    Added fields and properties

--- Memory.cs
+++ Memory.cs
@@ -1,3 +1,29 @@
 ﻿using System.Numerics;
+using CryptoProject.CryptoSystems;
 namespace CryptoProjectMemory;
 
+class Memory
+{
+    #region fields
+    List<CryptoSystem> _systemMemory = new List<CryptoSystem>();
+    List<Message> _messageMemory = new List<Message>();
+    #endregion
+    #region properties
+    public List<CryptoSystem> systemMemory
+    {
+        get { return _systemMemory; }
+        set { _systemMemory = value; }
+    }
+    public List<Message> messageMemory
+    {
+        get { return _messageMemory; }
+        set { _messageMemory = value; }
+    }
+    #endregion
+    #region constructors
+
+    #endregion
+    #region methods
+
+    #endregion
+}
