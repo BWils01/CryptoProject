cd6016153e159eacc816c13cba09832b0cfe0efd
Benjamin Wilson <Ben.Wilson@trojans.dsu.edu>
2022-05-05 18:34:10 -0500

    Added regions

--- CryptoSystems/CryptoSystem.cs
+++ CryptoSystems/CryptoSystem.cs
@@ -3,9 +3,11 @@ namespace CryptoProject.CryptoSystems;
 
 abstract class CryptoSystem
 {
+    #region fields
     protected BigInteger _prime;
     protected string _system;
-
+    #endregion
+    #region properties
     public BigInteger prime
     {
         get { return _prime; }
@@ -14,8 +16,14 @@ abstract class CryptoSystem
     {
         get { return _system; }
     }
+    #endregion
+    #region constructors
+
+    #endregion
+    #region methods
     public abstract void PrintInfo();
     public abstract string Encrypt(string message);
     public abstract string Decrypt(string message);
     public abstract string Intercept(string message);
+    #endregion
 }
